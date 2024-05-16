da266115f8d6f185d2d3b935bbaa38ace07a43cc
harshit2024 <harshitgsrivastava07@gmail.com>
2024-05-16 07:38:42 +0530

    local mysql db changed to RDS mysql db

--- src/main/java/com/springboot/blog/Blogwebsiterestapi/BlogWebsiteRestApiApplication.java
+++ src/main/java/com/springboot/blog/Blogwebsiterestapi/BlogWebsiteRestApiApplication.java
@@ -1,9 +1,13 @@
 package com.springboot.blog.Blogwebsiterestapi;
 
+import com.springboot.blog.Blogwebsiterestapi.entity.Role;
+import com.springboot.blog.Blogwebsiterestapi.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -23,7 +27,7 @@ import org.springframework.context.annotation.Bean;
 )
 public class
 
-BlogWebsiteRestApiApplication {
+BlogWebsiteRestApiApplication implements CommandLineRunner{
 
    @Bean
    public ModelMapper modelMapper(){
@@ -31,5 +35,20 @@ BlogWebsiteRestApiApplication {
    }
 	public static void main(String[] args) {
 		SpringApplication.run(BlogWebsiteRestApiApplication.class, args);
+	}
+
+	@Autowired
+	private RoleRepository roleRepository;
+	@Override
+	public void run(String... args) throws Exception {
+	     Role adminRole=new Role();
+		 adminRole.setName("ROLE_ADMIN");
+		 roleRepository.save(adminRole);
+
+		Role userRole=new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+
 	}
 }
